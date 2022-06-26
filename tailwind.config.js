13104743d09b26c7e605d0554f47c2530a436140
iury nadin <iurynadin@gmail.com>
2022-06-26 18:46:29 -0300

    seção Onde Estamos

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,7 @@ module.exports = {
             },
             backgroundImage: {
                 quemsomos: "url('../svgs/bg-quemsomos.svg')",
+                ondeestamos: "url('../svgs/bg-ondeestamos.svg')",
             },
             boxShadow: {
             },
