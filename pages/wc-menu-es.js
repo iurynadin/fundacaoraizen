073f961dd2e6ab7d5521ab25bb766a3d26e69cdb
Iury Nadin <iury@noctuaart.com.br>
2022-06-29 18:27:15 -0300

    espanhol

---
+++ pages/wc-menu-es.js
@@ -0,0 +1,18 @@
+class MenuElement extends HTMLElement {
+    constructor() {
+        super(); 
+    }
+    connectedCallback() {
+        this.innerHTML = `
+            <li><a href="#quem_somos">1. Quem Somos </a> </li>
+            <li><a href="#destaques">2. Destaques do ano</a> </li>
+            <li><a href="#ondeestamos">3. Dónde estamos</a> </li>
+            <li><a href="#mensagens">4. Mensajes de los líderes</a> </li>
+            <li><a href="#programas">5. Programas</a> </li>
+            <li class="small"><a href="#ativainfancia">5.1 Ativa Infância</a> </li>
+            <li class="small"><a href="#ativajuventude">5.2 Ativa Juventude</a> </li>
+            <li><a href="#leiamais">6. Leer más</a> </li>
+        `;
+    }
+}
+window.customElements.define('wc-menu-es', MenuElement);  
