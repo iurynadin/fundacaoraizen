19841c2b877b101e836390c58d47090165c78ccf
Sérgio Lemos <40553107+sergiolemoscarvalho@users.noreply.github.com>
2022-07-07 12:14:36 -0300

    Inserção de PDF

--- pages/wc-downreport.js
+++ pages/wc-downreport.js
@@ -1,28 +1,28 @@
 class DownReportElement extends HTMLElement {
     constructor() {
-        super(); 
+        super();
     }
     connectedCallback() {
         const lang = this.getAttribute('lang');
         var label = '';
         var link = '';
-        if(lang == 'es') {
+        if (lang == 'es') {
             label = 'Descargue el <span>Informe de actividades</span>';
-            link = '#';
-        }else if(lang == 'en'){
+            link = './pdf/fundacao_raizen_relatorio_de_atividades_2021_ES.pdf';
+        } else if (lang == 'en') {
             label = 'Download <span>Activities report</span>';
-            link = '#';
+            link = './pdf/fundacao_raizen_relatorio_de_atividades_2021_EN.pdf';
         } else {
             label = 'Baixar o <span>Relatório de Atividades</span>';
-            link = '#';
+            link = './pdf/fundacao_raizen_relatorio_de_atividades_2021_PT.pdf';
         }
 
         this.innerHTML = `
-        <a href="#" class="secondaryNav__btn group">
+        <a href="${link}" class="secondaryNav__btn group" target="_blank" download>
             <img src="./svgs/ico-relatorio.svg" alt="ico relatorio">
             <div>${label}</div>
         </a>
         `;
     }
 }
-window.customElements.define('wc-downreport', DownReportElement);  
+window.customElements.define('wc-downreport', DownReportElement);
